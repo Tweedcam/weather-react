8a43d25051d0e05bfc7930261cc2d4da67ccb0d4
Tweedcam <74871349+Tweedcam@users.noreply.github.com>
2021-02-17 16:13:17 +0000

    Update App.js
    imported and added weather and city components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./styles.css";
 import Search from "./Search.js";
+import City from "./City";
+import Weather from "./Weather.js";
+
 
 
 
@@ -8,6 +11,7 @@ export default function App() {
   return (
     <div className="App" className="wrapper">
       < Search />
+      <Weather /> <City />
       
     </div>
   );
