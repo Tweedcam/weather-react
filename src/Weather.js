3f172d6b79b05547ae8486941f869ae6b53de5fd
Tweedcam <74871349+Tweedcam@users.noreply.github.com>
2021-02-17 16:10:36 +0000

    Update Weather.js
    amended className

--- src/Weather.js
+++ src/Weather.js
@@ -5,7 +5,7 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="weather">
-      <i class="fas fa-cloud fa-3x"></i>
+      <i className="fas fa-cloud fa-3x"></i>
       <span className="summary">Cloudy</span>
       <span className="feels">feels like 2°C</span>
       <span className="humidity">Humidity:40%</span>
