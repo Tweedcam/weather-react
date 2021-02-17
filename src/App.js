9bcf7df4a7c0ce7dd5d3f81bf1f5b1ff0972b067
Tweedcam <74871349+Tweedcam@users.noreply.github.com>
2021-02-17 15:52:05 +0000

    Update App.js
    added search component to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import "./styles.css";
+import Search from "./Search.js";
 
 
 
 export default function App() {
   return (
     <div className="App" className="wrapper">
+      < Search />
       
     </div>
   );
