99c06586a314e9def3e63d8a3eeb069685a63d30
Tweedcam <74871349+Tweedcam@users.noreply.github.com>
2021-02-21 12:03:34 +0000

    Update Search.js
    rebuilt the search form and removed location button

--- src/Search.js
+++ src/Search.js
@@ -3,31 +3,19 @@ import "./Search.js";
 
 export default function Search() {
   return (
-    <div className="Search">
-      <form id="search-form" className="searchForm" className="col-sm">
-        <input
-          type="text"
-          id="city-input"
-          placeholder="Enter City..."
-          autofocus="on"
-          autocomplete="off"
-          aria-label="default input example"
-        />
-        <input
-          type="submit"
-          value="search"
-          autofocus="on"
-          className="btn btn-primary"
-        />
-        <input
-          type="submit"
-          value="Location"
-          autofocus="on"
-          id="current-location"
-          className="pinpoint"
-          className="btn btn-success"
-        />
+    <div className="search">
+      <form>
+        <div className="row">
+          <div className="col-6">
+        <input type="search" placeholder="Enter a city" className="form-control"/>
+        </div>
+        <div className="col-3">
+        <input type="submit" value="search" className="btn btn-primary"/>
+        </div>
+        </div>
       </form>
+
+
     </div>
   );
 }
