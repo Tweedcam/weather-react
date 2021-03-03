0a34b6ac343a36134d817be6b37124be2f888e81
Tweedcam <74871349+Tweedcam@users.noreply.github.com>
2021-03-03 11:20:55 +0000

    Update WeatherInfo.js
    added div and row and col to improve layout

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -5,14 +5,10 @@ import TemperatureConversion from "./TemperatureConversion";
 export default function WeatherInfo(props){
 
 return(
-< div className="WeatherInfo" >      
-<h1 className="city">{props.data.name}</h1>
-      <span className="dateTime">
-        <FormattedDate date={props.data.date} />
-        </span>
-        <TemperatureConversion celsius={props.data.temperature} />
-    
-    <div className="weather">
+< div className="WeatherInfo" className="row">      
+
+ 
+<div className="weather" className="col-6">
     <img src={props.data.iconUrl} alt={props.data.description} className="weatherIcon"/>
     <ul className="summary">
     <li className="description"> {props.data.description} </li>
@@ -21,6 +17,16 @@ return(
     <li className="wind">{Math.round(props.data.wind)} km/ph</li>
     </ul>
   </div>
+
+<div className="col-6">
+<h1 className="city" >{props.data.name}</h1>
+      <span className="dateTime">
+        <FormattedDate date={props.data.date} />
+        </span>
+        <TemperatureConversion celsius={props.data.temperature} />
+</div>
+
+   
 </div>
 
 
