648b19d4c89ac87081f4161a4031777ba861902d
Tweedcam <74871349+Tweedcam@users.noreply.github.com>
2021-03-05 13:00:54 +0000

    Update ForecastInfo.js
    created functions to handle data in component

--- src/ForecastInfo.js
+++ src/ForecastInfo.js
@@ -2,11 +2,28 @@ import React from "react";
 
 export default function ForecastInfo(props){
     let icon=props.data.weather[0].icon;
+
+    function hours(){
+        let date=new Date(props.data.dt*1000);
+        let hours=date.getHours();
+        if(hours<10){
+            hours=`0${hours}`;
+        }
+        return`${hours}:00`;
+        
+    }
+
+
+    function temperature(){
+        let temperature= Math.round(props.data.temp);
+        return `${temperature}°C`;
+    }
+
     return(
         <div className="forecastInfo col">
-        {new Date(props.data.dt*1000).getHours()}:00
+            {hours()}
         <img src={`https://openweathermap.org/img/wn/${icon}@2x.png`}/>
-                {Math.round(props.data.temp)}°C 
+                {temperature()}
                 </div>
     );
 }
